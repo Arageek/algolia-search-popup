f8e2dc93ec7adecc87ff922cc7415d55523d7d28
devwael <dev.ahmedwael@gmail.com>
2021-08-22 18:45:51 +0000

    initial files

---
+++ algolia-search-popup.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Algolia search popup
+ * Plugin URI: https://clearfy.pro
+ * Description: Disables unused Wordpress features, improves performance and increases SEO rankings, using Clearfy, which makes WordPress very easy.
+ * Author: Creative Motion <info@cm-wp.com>
+ * Version: 1.9.4
+ * Text Domain: clearfy
+ * Domain Path: /languages/
+ * Author URI: https://cm-wp.com
+ * Framework Version: FACTORY_449_VERSION
+ */
