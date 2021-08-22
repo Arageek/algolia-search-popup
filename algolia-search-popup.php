ba10d8e8d9101ce5ad3386cfabd4d879156e5440
Ahmad Wael <dev.ahmedwael@gmail.com>
2021-08-22 20:42:05 +0000

    run plugin

--- algolia-search-popup.php
+++ algolia-search-popup.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Algolia search popupgit
+ * Plugin Name: Algolia search popup
  * Plugin URI: https://github.com/Arageek/algolia-search-popup
  * Description: Add popup search form compatible with algolia autocomplete and wp search
  * Author: Ahmad Wael
@@ -10,6 +10,14 @@
  * Author URI: https://github.com/devwael
  */
 
-if(!defined('ABSPATH')){
-    wp_die('Not Allowed!');
+if ( ! defined( 'ABSPATH' ) ) {
+	wp_die( 'Not Allowed!' );
 }
+
+define( 'ASP_VERSION', '1.0.0' );
+define( 'ASP_PATH', plugin_dir_path( __FILE__ ) );
+define( 'ASP_URI', plugin_dir_url( __FILE__ ) );
+
+require ASP_PATH . 'includes/Initiator.php';
+
+Jamalnow_ASP\Initiator::get_instance(); //run plugin core
