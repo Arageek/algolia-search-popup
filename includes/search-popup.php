54d123be18522b44a9fd22f317f189f4f912263a
Ahmad Wael <dev.ahmedwael@gmail.com>
2021-08-23 11:17:39 +0000

    add search icon and adjust form structure

--- includes/search-popup.php
+++ includes/search-popup.php
@@ -12,8 +12,14 @@ class Search_Popup {
 			<div class="asp-popup-area-wrapper" hidden>   
 				<form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
 					<div class="asp-search-inputs">
-						<input type="search" name="s" class="asp-popup-search-input">
-						<button type="submit" class="asp-popup-submit-button">Go</button>
+						<label for="asp-search-input-text">
+							<svg viewBox="0 0 20 20" fill="currentColor" class="flex-none h-24 py-1"><path d="M19.71,18.29,16,14.61A9,9,0,1,0,14.61,16l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,19.71,18.29ZM2,9a7,7,0,1,1,12,4.93h0s0,0,0,0A7,7,0,0,1,2,9Z"></path></svg>
+						</label>
+						<div class="asp-popup-search-input-wrapper">
+							<input type="search" id="asp-search-input-text" name="s" class="asp-popup-search-input" placeholder="إبحث عن عملية او إجراء">
+						</div>
+						
+						<button type="button" class="asp-close-search-popup asp-search-closer">إلغاء</button>
 					</div>
 				</form>
 				<div class="asp-search-overlay asp-search-closer"></div>
